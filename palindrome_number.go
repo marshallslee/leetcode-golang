e12be1a7a228afbfb4d7255c60648912a2e68a7f
Marshall <marshall@soomgo.com>
2020-03-11 12:22:09 +0900

    palindrome_number.go

--- palindrome_number.go
+++ palindrome_number.go
@@ -3,10 +3,29 @@
 
 package main
 
-func isPalindrome(x int) bool {
+import "fmt"
 
+func isPalindrome(x int) bool {
+	var remainder, temp int
+	reverse := 0
+
+	temp = x
+
+	for {
+		remainder = x % 10
+		reverse = reverse*10 + remainder
+		x /= 10
+
+		if x == 0 {
+			break
+		}
+	}
+
+	return temp == reverse
 }
 
 func main() {
-
+	num := 10001
+	palindrome := isPalindrome(num)
+	fmt.Println(palindrome)
 }
